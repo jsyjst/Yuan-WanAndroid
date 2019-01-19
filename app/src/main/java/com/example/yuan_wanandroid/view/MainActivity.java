c179b173056c5cbd3aa3fb73ed8f792863a5a710
jsyjst <y8588130@163.com>
2019-01-19 16:10:27 +0800

    使用gradle统一依赖管理，添加了dagger2+retrofit+rxjava2+butterknife的依赖

---
+++ app/src/main/java/com/example/yuan_wanandroid/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yuan_wanandroid.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yuan_wanandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

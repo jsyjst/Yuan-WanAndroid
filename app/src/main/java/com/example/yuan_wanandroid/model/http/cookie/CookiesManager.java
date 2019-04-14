eeddd45527ea86d141058b3ac291ce4f790bfe14
jsyjst <y8588130@163.com>
2019-04-14 13:24:33 +0800

    实现登录持久化，删除之前采取的拦截器读取cookie的方法，改用okhttp中cookie的自动管理

---
+++ app/src/main/java/com/example/yuan_wanandroid/model/http/cookie/CookiesManager.java
@@ -0,0 +1,12 @@
+package com.example.yuan_wanandroid.model.http.cookie;
+
+/**
+ * <pre>
+ *     author : 残渊
+ *     time   : 2019/04/14
+ *     desc   :
+ * </pre>
+ */
+
+public class CookiesManager {
+}
